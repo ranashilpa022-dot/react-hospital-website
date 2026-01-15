008d80bf561194330c0c64c7597531a837ee43bf
ranashilpa022-dot <ranashilpa022@gmail.com>
2026-01-15 10:01:30 +0530

    Added animations across all pages

--- src/App.js
+++ src/App.js
@@ -1,31 +1,4 @@
-// import React from "react";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Navbar from "./components/Navbar";
-// import Home from "./pages/Home";
-// import About from "./pages/About";
-// import Doctors from "./pages/Doctors";
-// import Contact from "./pages/Contact";
-// import Appointment from "./pages/Appointment";
-// import Footer from "./components/Footer";
-
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/about" element={<About />} />
-//         <Route path="/doctors" element={<Doctors />} />
-//         <Route path="/contact" element={<Contact />} />
-//         <Route path="/appointment" element={<Appointment />} />
-        
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
+// animation test commit
 
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
